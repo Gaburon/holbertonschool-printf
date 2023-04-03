ea2e5073b27b6ef50f5ad670aa69fea8d068f296
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-04-03 11:42:22 -0500

    Update main.h

--- main.h
+++ main.h
@@ -18,8 +18,6 @@ typedef struct cases
 } print_cases;
 
 char *_itoa(int num, char *s, unsigned int base);
-char *_utoa(unsigned int num, char *s, unsigned int base);
-char *_utoah(unsigned int num, char *s, unsigned int base);
 int _printf(char *format, ...);
 int replace(char *buffer, char *s, int print_len);
 int (*get_print_cases(char *arg))(va_list, char *, int);
@@ -27,7 +25,6 @@ int c_case(va_list, char *, int);
 int s_case(va_list, char *, int);
 int perc_case(va_list, char *, int);
 int dec_case(va_list, char *, int);
-int bin_case(va_list, char *, int);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
